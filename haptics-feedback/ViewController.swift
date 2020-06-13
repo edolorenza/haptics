b12567fb963a4443587d1cbd89aa79d89e9cc4f2
edo lorenza <edolrnz@gmail.com>
2020-06-13 14:34:14 +0700

    give button action

--- haptics-feedback/ViewController.swift
+++ haptics-feedback/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let notification = UINotificationFeedbackGenerator()
+    let impactLight = UIImpactFeedbackGenerator(style: .light)
+    let impactMedium = UIImpactFeedbackGenerator(style: .medium)
+    let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
+     let selection = UISelectionFeedbackGenerator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnSuccessTap(_ sender: Any) {
+         notification.notificationOccurred(.success)
+    }
+    
+    @IBAction func btnWarningTap(_ sender: Any) {
+         notification.notificationOccurred(.warning)
+    }
+    
+    @IBAction func btnErrorTap(_ sender: Any) {
+          notification.notificationOccurred(.error)
+    }
+    
+    @IBAction func btnLightTap(_ sender: Any) {
+        self.impactLight.impactOccurred()
+    }
+    
+    @IBAction func btnMediumTap(_ sender: Any) {
+          self.impactMedium.impactOccurred()
+    }
+    
+    @IBAction func btnHeavyTap(_ sender: Any) {
+          self.impactHeavy.impactOccurred()
+    }
+    
+    @IBAction func btnSelectionTap(_ sender: Any) {
+        selection.selectionChanged()
+    }
 }
 
